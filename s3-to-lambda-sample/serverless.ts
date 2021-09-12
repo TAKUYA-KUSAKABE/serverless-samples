8d18ed92160a862ed7de193c357bac49c86e2e3d
pilefort <pilefort2020@gmail.com>
2021-09-12 18:07:15 +0900

    add put s3 sample

--- s3-to-lambda-sample/serverless.ts
+++ s3-to-lambda-sample/serverless.ts
@@ -1,9 +1,7 @@
 import type { AWS } from '@serverless/typescript';
 
-import hello from '@functions/hello';
-
 const serverlessConfiguration: AWS = {
-  service: 's3-to-lambda-sample',
+  service: 's3-to-lambda-sample-0912',
   frameworkVersion: '2',
   custom: {
     webpack: {
@@ -11,22 +9,21 @@ const serverlessConfiguration: AWS = {
       includeModules: true,
     },
   },
-  plugins: ['serverless-webpack', 'serverless-dotenv-plugin', 'serverless-offline'],
+  plugins: ['serverless-webpack', 'serverless-offline', 'serverless-dotenv-plugin'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
     region: 'ap-northeast-1',
-    apiGateway: {
-      minimumCompressionSize: 1024,
-      shouldStartNameWithService: true,
-    },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
     },
     lambdaHashingVersion: '20201221',
   },
-  // import the function via paths
-  functions: { hello },
+  functions: {
+    main: {
+      handler: './handler.main'
+    }
+  },
 };
 
 module.exports = serverlessConfiguration;
